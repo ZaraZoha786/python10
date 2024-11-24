562fb8c59b63eea87166d8c7dd23fbcb56d846af
ZaraZoha786 <mzarabha@gmail.com>
2024-11-24 15:02:47 +0200

    cube

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ def total_calc(bill_amount,tip_perc):
   print(f"please pay ${total}")
 
 total_calc(150,20)
+
+
+def cube(number):
+  return number*number*number
+ 
+def by_three(number):
+  if number %3 ==0:
+    return cube(number)
+  else:
+    return False
+print(by_three(9))
+print(by_three(4))
